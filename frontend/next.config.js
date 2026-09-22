1e105d0b5cc432a980138a3d9f99932343ce08fb
Alan de O. Gonçalves <alandeogoncalves@gmail.com>
2026-09-22 15:26:09 -0300

    fix: update backend hostname in remote image patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "http", hostname: "localhost", port: "8000" },
       { protocol: "http", hostname: "127.0.0.1", port: "8000" },
-      { protocol: "https", hostname: "p01--backend--6xggq4ccj4b6.code.run" },
+      { protocol: "https", hostname: "p01--backend--9l6dvd9xzxnm.code.run" },
       { protocol: "https", hostname: "pub-3665e297a9094e04a5761e404ef4a7cb.r2.dev" },
     ],
   },
